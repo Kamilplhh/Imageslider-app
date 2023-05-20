5fc90753910dcd083b3798b6b86b8427bead89e4
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-20 19:48:55 +0200

    Navbar done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,5 @@
 @section('title', '- Home')
 
 @section('content')
-<div class="bg-slate-800 dark:bg-white rounded-lg px-12 py-12 ring-slate-900/5 shadow-xl">
-    <i class="sun fa-solid fa-sun fa-xl" style="color: #000000;"></i>
-    <i class="moon fa-solid fa-moon fa-xl" style="color: #df4949;"></i>
-    <div class="bg-white dark:bg-black rounded-lg px-12 py-12 ring-slate-400/5 shadow-xl">
-</div>
+<div class="block"></div>
 @stop
