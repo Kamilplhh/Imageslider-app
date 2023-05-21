1e1141362eba991b219dfe625413efda55e79758
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-21 23:09:16 +0200

    Added photos with description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,25 @@
 
 @section('content')
 <div class="bg-blocks">
-    <div class="bg-block bg-black">
+    <div class="bg-block bg-black" id="1">
+        Elizeu Dias · BRASIL
+        RIO DE JANEIRO
     </div>
-    <div class="bg-block bg-black">
+    <div class="bg-block bg-black" id="2">
+        Portuguese Gravity · PORTUGAL
+        PADRÃO DOS DESCOBRIMENTOS IN BELÉM
     </div>
-    <div class="bg-block bg-black">
+    <div class="bg-block bg-black" id="3">
+        Nick Fewings · DUBIA
+        JUMEIRAH BEACH
     </div>
-    <div class="bg-block-last bg-black">
+    <div class="bg-block bg-black" id="4">
+        Izuddin Helmi Adnan · MALAYSIA
+        PENANG FISHERMAN JETTY
+    </div>
+    <div class="bg-block last bg-black" id="5">
+        Jacob Creswick · WASHINGTON
+        WASHINGTON MONUMENT
     </div>
 </div>
 
