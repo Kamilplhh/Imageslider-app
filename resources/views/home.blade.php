8a860fed870d01af2df83138318a8c4e035d0ad4
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-21 23:45:45 +0200

    Page css progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,38 @@
 
 @section('content')
 <div class="bg-blocks">
-    <div class="bg-block bg-black" id="1">
-        Elizeu Dias · BRASIL
+    <div class="bg-block bg-black" id="first">
+        <div class="author">
+            Elizeu Dias · BRASIL
+        </div>
         RIO DE JANEIRO
     </div>
-    <div class="bg-block bg-black" id="2">
-        Portuguese Gravity · PORTUGAL
+    
+    <div class="bg-block bg-black" id="second">
+        <div class="author">
+            Portuguese Gravity · PORTUGAL
+        </div>
         PADRÃO DOS DESCOBRIMENTOS IN BELÉM
     </div>
-    <div class="bg-block bg-black" id="3">
-        Nick Fewings · DUBIA
+
+    <div class="bg-block bg-black" id="third">
+        <div class="author">
+            Nick Fewings · DUBIA
+        </div>
         JUMEIRAH BEACH
     </div>
-    <div class="bg-block bg-black" id="4">
-        Izuddin Helmi Adnan · MALAYSIA
+
+    <div class="bg-block bg-black" id="fourth">
+        <div class="author">
+            Izuddin Helmi Adnan · MALAYSIA
+        </div>
         PENANG FISHERMAN JETTY
     </div>
-    <div class="bg-block last bg-black" id="5">
-        Jacob Creswick · WASHINGTON
+
+    <div class="bg-block last bg-black" id="fifth">
+        <div class="author">
+            Jacob Creswick · WASHINGTON
+        </div>
         WASHINGTON MONUMENT
     </div>
 </div>
